d19c57b958012117f639144efd02c718b96514dc
adityagithubraj <adityaraj9843@gmail.com>
2025-04-07 08:06:05 +0530

    Made changes and updated the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//    base : "/khushi-gutpta-portfolio/",
+//   optimizeDeps: {
+//     exclude: ['lucide-react'],
+//   },
+// });
+
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/khushi-gupta-portfolio/', // 👈 very important
   plugins: [react()],
-   base : "/khushi-gutpta-portfolio/",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
